a331bb54fb05ed9e1d56a88f646bdd2cc3b16233
Carlos Alexandre <alexfilho062005@gmail.com>
2024-12-23 18:36:32 -0300

    :1234: feat: formatter date and price

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 import { SummaryCard, SummaryContainer } from './styles';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { useContext } from 'react';
+import { princeFormatter } from '../../utils/formatter';
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
@@ -33,7 +34,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{princeFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -42,7 +43,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{princeFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -51,7 +52,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{princeFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
