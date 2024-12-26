21a4e9dba19894fd3c57d04058030e316875cbe8
Carlos Alexandre <alexfilho062005@gmail.com>
2024-12-26 11:16:27 -0300

    :heavy_plus_sign: feat: add use-con text-selector

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -11,8 +11,9 @@ import * as z from 'zod';
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
-import { useContext } from 'react';
+
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { useContextSelector } from 'use-context-selector';
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -30,7 +31,9 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({
   handleTransactionModalOpenChange,
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const  createTransaction  = useContextSelector(TransactionsContext, (context) => {
+    return context.createTransaction
+  });
   const {
     control,
     register,
